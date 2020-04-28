ad7de0c3b8bdf19d082e53de4f834cbdd38cd4d2
Danica Valdez <danicaevaldez@gmail.com>
2020-04-27 20:43:23 -0700

    removed import in App.js to avoid merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
-import ApplicantProfile from "./components/applicant-profile/ApplicantProfile";
+
 
 function App() {
   return (
     <div className="App">
-      <ApplicantProfile />
     </div>
   );
 }
--- src/components/applicant-profile/ApplicantProfile.js
+++ src/components/applicant-profile/ApplicantProfile.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from './node_modules/react'
 import BioCard from './BioCard'
 import GrantsList from './grants-list/Grants'
 
