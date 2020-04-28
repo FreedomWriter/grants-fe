96bcf9a0954c4afc87059a2f1a0198a4bc18078b
Toosdai Otte <toosdai52Otte@gmail.com>
2020-04-27 22:42:25 -0500

    layout/frontend styling for register form done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import { BrowserRouter as Router} from 'react-router-dom';
+
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
@@ -13,7 +15,9 @@ ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <PersistGate persistor={persistor}>
-        <App />
+        <Router>
+          <App />          
+        </Router>
       </PersistGate>
     </React.StrictMode>
   </Provider>,
