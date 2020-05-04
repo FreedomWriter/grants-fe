671e6934ed1ca5aa338f12872517c27d6e16ce6a
FreedomWriter <natalienetworks@gmail.com>
2020-05-04 11:37:24 -0500

    debugged api call for colleges

--- src/store/actions/collegeActions.js
+++ src/store/actions/collegeActions.js
@@ -7,34 +7,35 @@ export const COLLEGES_GET_FAILURE = "COLLEGES_GET_FAILURE";
 export const CLEAR_COLLEGE_LIST = "CLEAR_COLLEGE_LIST";
 
 export const getColleges = (value) => (dispatch) => {
-  dispatch({ type: COLLEGES_GET_START });
-  //FAKE RETURN TO AVOID API CALLS
-  dispatch({
-    type: COLLEGES_GET_SUCCESS,
-    payload: [{ id: 102377, name: "Tuskegee University" }],
-  });
+  console.log({ value });
+  dispatch({ type: COLLEGES_GET_START, payload: value });
+  // //FAKE RETURN TO AVOID API CALLS
+  // dispatch({
+  //   type: COLLEGES_GET_SUCCESS,
+  //   payload: [{ id: 102377, name: "Tuskegee University" }],
+  // });
 
   /* *********************** AXIOS COMMENTED OUT TO AVOID API CALLS DURING DEVELPMENT *********************** */
 
-  // return axios
-  //   .get(
-  //     `${process.env.REACT_APP_COLLEGE_API}${value}${process.env.REACT_APP_COLLEGE_API_KEY}`
-  //   )
-  //   .then((res) => {
-  //     const formatted = res.data.results.map((result) => {
-  //       return { id: result.id, name: result["school.name"] };
-  //     });
-  //     dispatch({
-  //       type: COLLEGES_GET_SUCCESS,
-  //       payload: formatted,
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     dispatch({
-  //       type: COLLEGES_GET_FAILURE,
-  //       payload: { error: err.message },
-  //     });
-  //   });
+  return axios
+    .get(
+      `${process.env.REACT_APP_COLLEGE_API}${value}${process.env.REACT_APP_COLLEGE_API_QUERY}${process.env.REACT_APP_COLLEGE_API_KEY}`
+    )
+    .then((res) => {
+      const formatted = res.data.results.map((result) => {
+        return { id: result.id, name: result["school.name"] };
+      });
+      dispatch({
+        type: COLLEGES_GET_SUCCESS,
+        payload: formatted,
+      });
+    })
+    .catch((err) => {
+      dispatch({
+        type: COLLEGES_GET_FAILURE,
+        payload: { error: err.message },
+      });
+    });
 };
 
 export const clearCollegeList = () => (dispatch) => {
