8be6c2ab321cc5061a4fd004eef2d406caf9baa6
Toosdai Otte <toosdai52Otte@gmail.com>
2020-05-04 17:09:41 -0500

    added validation for inputs

--- src/components/register/registerForm.js
+++ src/components/register/registerForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -8,11 +8,12 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import { makeStyles } from '@material-ui/core/styles';
 import { axiosWithAuth } from '../../utils/axiosWithAuth';
+
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -30,73 +31,124 @@ const useStyles = makeStyles((theme) => ({
     },
     formControl: {
         margin: theme.spacing(1),
-        width: '100em',
+        minWidth: 395,
     },
     selectEmpty: {
         marginTop: theme.spacing(2),
     },
 }));
 
-export default function RegisterForm() {
+
+export default function RegisterForm(props) {
+
     const classes = useStyles();
-    const [userType, setUserType] = React.useState('');
+
+    const [ values, setValues ] = useState({
+            firstName: '',
+            lastName: '',
+            email: '',
+            userType: '',
+            password: '',
+            confirmPassword: '',           
+
+    })
+
+    ValidatorForm.addValidationRule('isPasswordMatch', (value) => {
+        if(value !== values.password){
+            return false;
+            console.log(value)
+        }
+        return true
+    });
 
     const handleChange = (e) => {
-        setUserType(e.target.value);
-    };
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });            
+        }
+
+    const handleSelectChange = (e) => {
+        setValues({
+            userType: e.target.value 
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axiosWithAuth()
+            .post('api url', values)
+            .then(res => {
+                props.history.push('/')
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        console.log(values)
+    }
 
     return (
-        <div class='register'>
+        <div className='register'>
             <Container component="main" maxWidth="xs">
                 <CssBaseline />
                 <div className={classes.paper}>
                     <Typography component="h1" variant="h5">
                         Create Your Account
                     </Typography>
-                    <form className={classes.root} autoComplete="off">
+                    <ValidatorForm className={classes.root} autoComplete="off" onSubmit={handleSubmit}>
                     <Grid container spacing={2}>
                         <Grid item xs={12} sm={6}>
-                            <TextField
+                            <TextValidator
                                 autoComplete="fname"
                                 name="firstName"
                                 variant="outlined"
-                                required
+                                value={values.firstName}
+                                validators={['required']}
+                                errorMessages={['This field is required']}
                                 fullWidth
-                                id="firstName"
                                 label="First Name"
                                 autoFocus
+                                onChange={handleChange}
                             />
+
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <TextField
+                            <TextValidator
                                 variant="outlined"
-                                required
                                 fullWidth
                                 id="lastName"
                                 label="Last Name"
                                 name="lastName"
+                                value={values.lastName}
+                                validators={['required']}
+                                errorMessages={['This field is required']}
                                 autoComplete="lname"
+                                onChange={handleChange}
                             />
+
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField
+                            <TextValidator
                                 variant="outlined"
-                                required
                                 fullWidth
-                                id="email"
-                                label="Email Address"
+                                label="Email"
                                 name="email"
-                                autoComplete="email"
+                                value={values.email}
+                                validators={['required', 'isEmail']}
+                                errorMessages={['This field is required', 'email is not valid']}
+                                onChange={handleChange}
                             />
+
                         </Grid>
                         <FormControl variant="outlined" className={classes.formControl}>
                             <InputLabel id="user-type">User Type</InputLabel>
                             <Select
-                                labelId="user-types-label"
-                                id="user-types"
-                                value={userType}
-                                onChangle={handleChange}
+                                labelId="user-type"
+                                required
+                                value={values.userType}
+                                onChange={handleSelectChange}
                                 label="User Type"
+                                // validators={['required']}
+                                // errorMessages={['This field is required']}
                             >
                                 <MenuItem value="">
                                     <em>Select User Type</em>
@@ -106,27 +158,29 @@ export default function RegisterForm() {
                             </Select>
                         </FormControl>
                         <Grid item xs={12}>
-                            <TextField
+                            <TextValidator
                                 variant="outlined"
-                                required
                                 fullWidth
                                 name="password"
                                 label="Password"
                                 type="password"
-                                id="password"
-                                autoComplete="current-password"
+                                value={values.password}
+                                validators={['required']}
+                                errorMessages={['This field is required']}
+                                onChange={handleChange}
                             />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField
+                            <TextValidator
                                 variant="outlined"
-                                required
                                 fullWidth
-                                name="confirm-password"
+                                name="confirmPassword"
                                 label="Confirm Password"
                                 type="password"
-                                id="confirm-password"
-                                autoComplete="current-password"
+                                value={values.confirmPassword}
+                                validators={['isPasswordMatch', 'required']}
+                                errorMessages={['Passwords do not match', 'This field is required']}
+                                onChange={handleChange}
                             />
                         </Grid>
                     </Grid>
@@ -146,7 +200,7 @@ export default function RegisterForm() {
                             </Link>
                         </Grid>
                     </Grid>
-                </form>  
+                </ValidatorForm>
             </div>        
             </Container>           
         </div>
