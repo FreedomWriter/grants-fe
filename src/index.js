082c8c11da84fbc30548f6dfdb919810e77e4bfd
FreedomWriter <natalienetworks@gmail.com>
2020-05-06 21:03:34 -0500

    npm install redux-mock-store --save-dev

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-import MomentUtils from "@date-io/moment";
-import * as serviceWorker from "./serviceWorker";
-
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
@@ -15,15 +11,8 @@ import { store, persistor } from "./store";
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <MuiPickersUtilsProvider utils={MomentUtils}>
-        <App />
-      </MuiPickersUtilsProvider>
+      <App />
     </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
