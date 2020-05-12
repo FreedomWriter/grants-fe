4ef62461ba5bb64b5990aef2344a4f59c0b364e2
Toosdai Otte <toosdai52Otte@gmail.com>
2020-05-11 21:00:34 -0500

    landing page styled

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./components/globals/theme";
 
 import Navbar from "./components/navbar/Navbar";
-import ApplicantProfileForm from "./components/ApplicantProfileForm/ApplicantProfileForm";
 
 function App() {
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <Navbar />
-        <ApplicantProfileForm />
       </ThemeProvider>
     </Router>
   );
