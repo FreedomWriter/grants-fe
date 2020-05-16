e02223015b5d8d3a5bfe0c2dd7be53235df6f1f6
FreedomWriter <natalienetworks@gmail.com>
2020-05-16 12:24:42 -0500

    updated axiosWithAuth to pull api from env variable

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
   const token = JSON.parse(localStorage.getItem("token"));
 
   return axios.create({
-    baseURL: "api goes here",
+    baseURL: process.env.REACT_APP_GRANTED_API,
     headers: {
       Authorization: token,
     },
