00babedf9afb17f214999fa6e2ebea478e467828
Brett Karr <b.karr@polyclip.us>
2020-05-17 13:20:04 -0500

    fixed some minor errors that occurred. Had to comment out line 29 and 66 to get site to load. Also added Links to the LandingPage so it directs to the Login/Register Forms

--- src/store/actions/ApplicantActions.js
+++ src/store/actions/ApplicantActions.js
@@ -13,58 +13,58 @@ export const GET_APPLICANTGRANTS_START = "GET_PROFILEINFO_START";
 export const GET_APPLICANTGRANTS_SUCCESS = "GET_PROFILEINFO_SUCCESS";
 export const GET_APPLICANTGRANTS_FAILURE = "GET_PROFILEINFO_FAILURE";
 
-export const getProfileInfo = applicant_id => dispatch => {
+export const getProfileInfo = (applicant_id) => (dispatch) => {
   dispatch({ type: GET_PROFILEINFO_START });
 
   axiosWithAuth()
     .get(/* insert api url here */)
-    .then(res => {
+    .then((res) => {
       dispatch({
         type: GET_PROFILEINFO_SUCCESS,
-        payload: res.data
+        payload: res.data,
       });
     })
-    .catch(err => {
+    .catch((err) => {
       dispatch({
-        type: GET_PROFILE_ERROR,
-        payload: "Cannot load profile information"
+        // type: GET_PROFILE_ERROR, //This isn't defined...
+        payload: "Cannot load profile information",
       });
     });
 };
 
-export const postApplicantGgrants = value => dispatch => {
+export const postApplicantGgrants = (value) => (dispatch) => {
   dispatch({ type: POST_APPLICANTGRANTS_START });
-    axiosWithAuth()
-      .post(/* insert api url here */)
-      .then((res) => {
-        dispatch({
-          type: POST_APPLICANTGRANTS_SUCCESS,
-          payload: value
-        });
-      })
-      .catch((err) => {
-        dispatch({
-          type: POST_APPLICANTGRANTS_FAILURE,
-          payload: { error: err.message },
-        });
+  axiosWithAuth()
+    .post(/* insert api url here */)
+    .then((res) => {
+      dispatch({
+        type: POST_APPLICANTGRANTS_SUCCESS,
+        payload: value,
       });
+    })
+    .catch((err) => {
+      dispatch({
+        type: POST_APPLICANTGRANTS_FAILURE,
+        payload: { error: err.message },
+      });
+    });
 };
 
-export const getApplicantGrants = applicant_id => dispatch => {
+export const getApplicantGrants = (applicant_id) => (dispatch) => {
   dispatch({ type: GET_APPLICANTGRANTS_START });
 
   axiosWithAuth()
     .get(/* insert api url here */)
-    .then(res => {
+    .then((res) => {
       dispatch({
         type: GET_APPLICANTGRANTS_SUCCESS,
-        payload: res.data
+        payload: res.data,
       });
     })
-    .catch(err => {
+    .catch((err) => {
       dispatch({
-        type: GET_APPLICANTGRANTS_ERROR,
-        payload: { error: err.message }
+        // type: GET_APPLICANTGRANTS_ERROR, // This isn't defined...
+        payload: { error: err.message },
       });
     });
 };
