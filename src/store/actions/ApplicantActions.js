ac52706646e2a2458d13f3d6f00e62cb2634dcb1
Danica Valdez <danicaevaldez@gmail.com>
2020-05-17 17:07:12 -0700

    Fixed naming convention for dispatches to be _FAILURE, insted of _ERROR

--- src/store/actions/ApplicantActions.js
+++ src/store/actions/ApplicantActions.js
@@ -27,7 +27,7 @@ export const getProfileInfo = (applicant_id) => (dispatch) => {
     })
     .catch((err) => {
       dispatch({
-        // type: GET_PROFILE_ERROR, //This isn't defined...
+        type: GET_PROFILE_FAILURE,
         payload: "Cannot load profile information",
       });
     });
@@ -64,7 +64,7 @@ export const getApplicantGrants = (applicant_id) => (dispatch) => {
     })
     .catch((err) => {
       dispatch({
-        // type: GET_APPLICANTGRANTS_ERROR, // This isn't defined...
+        type: GET_APPLICANTGRANTS_FAILURE.
         payload: { error: err.message },
       });
     });
