8c69e01176b0ede54f1fc1a4aa0db2815046a914
Brett Karr <b.karr@polyclip.us>
2020-05-18 22:07:46 -0500

    some homepage styling adjustments made, and some baseline testing setup for homepage

--- src/components/homepage/userCard/HomePageUserCardStyles.jsx
+++ src/components/homepage/userCard/HomePageUserCardStyles.jsx
@@ -1,5 +1,4 @@
 import { makeStyles, createStyles } from "@material-ui/core/styles";
-import { red } from "@material-ui/core/colors";
 
 export const useStyles = makeStyles((theme) =>
   createStyles({
