642454b3816a36bd58a98462385cf006ddb6ce28
Toosdai Otte <toosdai52Otte@gmail.com>
2020-05-27 18:02:37 -0500

    added landing page tests

--- src/components/landingPage/tests/LandingPage.test.js
+++ src/components/landingPage/tests/LandingPage.test.js
@@ -1,12 +1,8 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { render } from "@testing-library/react";
+import React from "react";
+import { BrowserRouter, Router } from 'react-router-dom';
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-
-import { MemoryRouter } from 'react-router-dom';
-import { act } from 'react-dom/test-utils';
+import { createMemoryHistory } from 'history';
 
 import LandingPage from "../LandingPage.js";
 
@@ -16,34 +12,50 @@ describe("Test", () => {
     });
   });
 
-  describe("LandingPage has buttons", () => {
-      test("LandingPage has register button", () => {
+describe("LandingPage has buttons", () => {
+    test("LandingPage has register button", () => {
         const { getByText } = render(
-            <Router>
+            <BrowserRouter>
             <LandingPage />
-            </Router>);
+            </BrowserRouter>);
         getByText(/Register/i);
       })
       test("LandingPage has login button", () => {
         const { getByText } = render(
-            <Router>
+            <BrowserRouter>
             <LandingPage />
-            </Router>);
+            </BrowserRouter>);
         getByText(/Login/i);          
       })
   })
 
-//   describe("Login/Register route to correct forms", () => {
-//       test("Register button routes to RegisterForm", () => {
-//         render(
-//             <MemoryRouter initialEntries={['/']}>
-//                 <LandingPage />
-//             </MemoryRouter>
-//         );
+  describe("Login/Register route to correct forms", async () => {
+      test("Register button routes to RegisterForm", () => {
 
-//         act(() => {
+        const history = createMemoryHistory();
 
-//         });
+        const { container, getByText } = render(
+            <Router history={history}>
+                <LandingPage />
+            </Router>
+        )
 
-//       })
-//   })
+        expect(container.innerHTML).toMatch('Get help with your grant today!')
+        fireEvent.click(getByText(/register/i))
+        expect(history.location.pathname).toBe("/RegisterForm");
+      })
+      test("Login button routes to LoginForm", () => {
+
+        const history = createMemoryHistory();
+
+        const { container, getByText } = render(
+            <Router history={history}>
+                <LandingPage />
+            </Router>
+        )
+
+        expect(container.innerHTML).toMatch('Get help with your grant today!')
+        fireEvent.click(getByText(/login/i))
+        expect(history.location.pathname).toBe("/LoginForm");
+      })
+  })
