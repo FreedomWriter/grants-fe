3d914d9ac58d9a3e0241e257d77f6bd2bbaccfb2
Brett Karr <b.karr@polyclip.us>
2020-06-12 12:06:27 -0500

    eff it

--- src/store/actions/grantsActions.js
+++ src/store/actions/grantsActions.js
@@ -15,7 +15,7 @@ export const PUT_GRANTS_FAILURE = "PUT_GRANTS_FAILURE";
 export const DELETE_GRANTS_START = "DELETE_GRANTS_START";
 export const DELETE_GRANTS_SUCCESS = "DELETE_GRANTS_SUCCESS";
 export const DELETE_GRANTS_FAILURE = "DELETE_GRANTS_FAILURE";
-  
+
 export const getGrants = () => (dispatch) => {
   dispatch({ type: GET_GRANTS_START });
   axiosWithAuth()
@@ -73,7 +73,6 @@ export const putGrants = (id, value) => (dispatch) => {
   axiosWithAuth()
     .put(`/grants/${id}`, value)
     .then((res) => {
-      console.log(res)
       dispatch({
         type: PUT_GRANTS_SUCCESS,
         payload: res.data,
