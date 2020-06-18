433f7a91b614771b6ad2e2922493d7cb704fb83d
FreedomWriter <natalienetworks@gmail.com>
2020-06-17 23:10:25 -0500

    added some styling

--- src/components/grantsPage/WritersFavoriteGrants.js
+++ src/components/grantsPage/WritersFavoriteGrants.js
@@ -2,9 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import GrantCard from "./grantCards/GrantCard.jsx";
+import { useStyles } from "./WritersFavoriteGrantsStyles";
 
 export default function WritersFavoriteGrants() {
   const faveArr = useSelector((state) => state.favorites.favorites);
+  const classes = useStyles();
 
   if (faveArr.length === 0) {
     return <Redirect to="/Grants" />;
@@ -12,7 +14,11 @@ export default function WritersFavoriteGrants() {
   return (
     <div>
       {faveArr.map((grant) => {
-        return <GrantCard key={grant.id} data={grant} />;
+        return (
+          <div className={classes.container}>
+            <GrantCard key={grant.id} data={grant} />
+          </div>
+        );
       })}
     </div>
   );
---
+++ src/components/grantsPage/WritersFavoriteGrantsStyles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: "50px 0 0 0",
+    width: "80%",
+    margin: "auto",
+  },
+}));
