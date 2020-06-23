8bccf799f88d3c505752e625b721ecc45b87e1df
Danica Valdez <danicaevaldez@gmail.com>
2020-06-22 20:12:55 -0700

    fixed where grants was being pulled from state so that edit and add functionality is working again.

--- src/components/applicant-profile/applicant-grants/UpdateGrant.js
+++ src/components/applicant-profile/applicant-grants/UpdateGrant.js
@@ -25,7 +25,7 @@ export default function UpdateGrant() {
   );
   const user_id = useSelector((state) => state.profileInfo.profileDetails.id);
   const grants = useSelector(
-    (state) => state.profileInfo.profileDetails.grants
+    (state) => state.grants.applicantGrants
   );
   const { id } = useParams();
 
