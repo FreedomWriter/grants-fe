03e14e6953108b25dc204ecdaa40964ff6e91c2e
FreedomWriter <natalienetworks@gmail.com>
2020-06-24 20:29:45 -0500

    added a header to favorite grants page

--- src/components/grantsPage/WritersFavoriteGrants.js
+++ src/components/grantsPage/WritersFavoriteGrants.js
@@ -3,9 +3,11 @@ import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import GrantCard from "./grantCards/GrantCard.jsx";
 import { useStyles } from "./WritersFavoriteGrantsStyles";
+import { Typography, Grid } from "@material-ui/core";
 
 export default function WritersFavoriteGrants() {
   const faveArr = useSelector((state) => state.favorites.favorites);
+  const user = useSelector((state) => state.profileInfo.profileDetails);
   const classes = useStyles();
 
   if (faveArr.length === 0) {
@@ -13,6 +15,14 @@ export default function WritersFavoriteGrants() {
   }
   return (
     <div>
+      <Typography variant="h3" className={classes.h3}>
+        <Grid container justify="center">
+          <Grid item>
+            {user.org_name !== "" ? user.org_name : user.first_name}'s Favorite
+            Grants
+          </Grid>
+        </Grid>
+      </Typography>
       {faveArr.map((grant) => {
         return (
           <div key={grant.id} className={classes.container}>
